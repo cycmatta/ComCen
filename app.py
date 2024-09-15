ea3ff3acabc13082f5cf5e6e83d7e7aa0120eb26
cycmatta <sts.matsuda@nifty.com>
2024-09-15 16:33:49 +0900

    エラーハンドリング適用

--- app.py
+++ app.py
@@ -22,7 +22,12 @@ st.markdown(
 #url = 'https://drive.google.com/uc?id=ファイルID'
 #url="https://drive.google.com/file/d/1Y7mPE4ikULQVi6T6TGDYlhz1nT2r-hXM/view?usp=sharing"
 url='https://drive.google.com/uc?id=1Y7mPE4ikULQVi6T6TGDYlhz1nT2r-hXM'
-df = pd.read_csv(url)
 
-# 表形式で表示
-st.write(df)
+try:
+    #encodingやエラーハンドリングを追加
+    df = pd.read_csv(url, error_bad_lines=False, warn_bad_lines=True)
+    # 表形式で表示
+    st.write(df)
+except pd.errors.ParserError as e:
+    st.error(f"CSVファイルの読み込みに失敗しました: {e}")
+    
