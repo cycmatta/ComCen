60e25acbed6f4a76c50566dd7da6b5f04c922bea
cycmatta <sts.matsuda@nifty.com>
2024-09-24 09:34:08 +0900

    タブ対応３

--- app.py
+++ app.py
@@ -31,11 +31,15 @@ st.subheader("みんなの声に力がわいてくる！")
 url1='https://drive.google.com/uc?id=1kYeIXoe0DC__s9oYqmM16M-YbWU_3W-s'
 #Q_町会マスタ_総合順位_繰上
 url2='https://drive.google.com/uc?id=1Sh8YcsmhfBTrWYgx-fz9E7Nb52e83LAg'
+#Q_大会記録
+url3='https://drive.google.com/uc?id=1GsecoyElkljs0pBIqexVJgUVYOKPeBMc'
+
 try:
     #encodingやon_bad_lines='skip'を使用して不正な行をスキップ
     df1 = pd.read_csv(url1, on_bad_lines='skip', encoding='utf-8')
     df2 = pd.read_csv(url2, on_bad_lines='skip', encoding='utf-8')
- 
+    df3 = pd.read_csv(url3, on_bad_lines='skip', encoding='utf-8')
+
     # df2の最新の更新日時を取得(更新日時を日付型に変換、最大値を取得、最大値を表示)
     df2['更新日時']=pd.to_datetime(df2['更新日時'])
     max_date=df2['更新日時'].max()
@@ -46,7 +50,7 @@ try:
     columns_order = ['総合順位', '町会NO', '町会', '繰越点','得点合計','総合計']  # 順番を指定
 
     # タブを作成
-    tab1, tab2 = st.tabs(["Tab 1", "Tab 2"])
+    tab1, tab2, tab3 = st.tabs(["総合順位", "クロス集計", "大会記録"])
 
     # タブごとに表形式で表示
     with tab1:
@@ -56,6 +60,10 @@ try:
     with tab2:
         st.write("大会記録のクロス集計")
         st.dataframe(df1)
+
+    with tab3:
+        st.write("大会記録")
+        st.dataframe(df3)
 
     # インデックスをリセットし、None/NaNを空文字に置き換え(うまく動作しないのでボツ)
     #df1_clean = df1.fillna("").reset_index(drop=True)
