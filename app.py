5b7f185ea5eb41eb3b9c67c6f5ff94fc3408b1f7
Satoshi Matsuda <sts.matsuda@nifty.com>
2024-09-24 21:18:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ try:
     df1 = pd.read_csv(url1, on_bad_lines='skip', encoding='utf-8')
     df2 = pd.read_csv(url2, on_bad_lines='skip', encoding='utf-8')
     df3 = pd.read_csv(url3, on_bad_lines='skip', encoding='utf-8')
-
+    #確認用
+    print(df3.columns)
     # df2の最新の更新日時を取得(更新日時を日付型に変換、最大値を取得、最大値を表示)
     df2['更新日時']=pd.to_datetime(df2['更新日時'])
     max_date=df2['更新日時'].max()
